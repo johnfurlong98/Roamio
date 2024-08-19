c462271be0df11842d83a67f3ecb50b4a1a878b8
John Furlong <furlongjohn088@gmail.com>
2024-08-19 13:37:04 +0000

    Added user profile view and URL configuration

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Destinations import views as index_views
+from users import views as users_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('profile/', users_views.profile, name='profile'),
+    path('hello', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
