9b1afaef6a7240ef5571d2e8f1eef7cb268ea02d
John Furlong <furlongjohn088@gmail.com>
2024-08-20 07:31:55 +0000

    started deployemnt to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-su%ekyw7$%x#34v3rm-l=1&1lm7v)g#nj@cmatb3@z5a-wv)-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-johnfurlong98-roamio-rxzjlylv44b.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-johnfurlong98-roamio-rxzjlylv44b.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
