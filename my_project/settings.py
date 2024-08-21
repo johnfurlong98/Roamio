efece5c0854fea9ba75cb4f780796d2c5c0fb3e3
John Furlong <furlongjohn088@gmail.com>
2024-08-21 13:30:35 +0000

    updated secret key eny.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-su%ekyw7$%x#34v3rm-l=1&1lm7v)g#nj@cmatb3@z5a-wv)-2'
+SECRET_KEY = os.environ.get("django-insecure-su%ekyw7$%x#34v3rm-l=1&1lm7v)g#nj@cmatb3@z5a-wv)-2")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
