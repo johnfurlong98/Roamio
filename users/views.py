19796cdc447416c54da63e4fab0b6faff2147670
John Furlong <furlongjohn088@gmail.com>
2024-08-28 14:16:18 +0000

    added favicon and updated styles on the destination form.hmtl

--- users/views.py
+++ users/views.py
@@ -4,9 +4,5 @@ from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
 
-# Create your views here.
-#def profile(request):
- #   return HttpResponse("This would be the user profile page")
-
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
